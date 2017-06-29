9df444974c61ccece703a11432e241cc4cca7bd5
Rodney Degracia <rodney@justecho.com>
2017-06-29 16:59:01 -0400

    Xcode 8.3.2 Swift 3.1

--- SwiftLanguageTests/Basics.swift
+++ SwiftLanguageTests/Basics.swift
@@ -6,19 +6,20 @@
 //  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
 //
 
+
 import XCTest
 
-class Basics: XCTestCase {
+class Basics : XCTestCase  {
 
-    func ary1()->Int[] {
+    func ary1()->[Int] {
         return [1,2,3]
     }
     
-    func ary2()->Int[] {
+    func ary2()->[Int] {
         return [1,2,3]
     }
     
-    func ary12()->Int[] {
+    func ary12()->[Int] {
         return (ary1() + ary2())
     }
     
@@ -42,9 +43,11 @@ class Basics: XCTestCase {
 
     func testConstantsAndVariables() {
         let a = 3
-        let a1: NSNumber = NSNumber(int: 3)
+        let a1: NSNumber = NSNumber(value: 3 as Int32)
         XCTAssert(a == 3, "a should equal 3")
-        XCTAssert(a1 == a, "a1 should equal a")
+        XCTAssert(a == a1.intValue, "a should equal 3")
+
+        //XCTAssert(a1 == a, "a1 should equal a")
         
         let b = "3"
         XCTAssert(b == "3", "b should equal 3")
@@ -83,17 +86,19 @@ class Basics: XCTestCase {
     
     func testTypeAnnotations() {
         
-        var a: Int = 1
-        var b : Int = 2
-        var c :Int = 3
+        let _: Int = 1
+        let _ : Int = 2
+        let _ :Int = 3
+        // XCTAssert(_ == _, "_ should equal _") ; ambigious use of operator ==
         
-        var a1: Int = Int(3.12)
-        var a2: Float = Float(3.12)
-        var a3: Double = Double(3.12)
-        
-        XCTAssert(a3 != a2, "Float(3.12) should not equal Double(3.12)")
-        XCTAssert(a1 != a3, "Int(3.12) should not equal Double(3.12)")
-        XCTAssert(a1 != a2, "Int(3.12) should not equal Float(3.12)")
+        let a1: Int = Int(3.12)
+        let a2: Float = Float(3.12)
+        let a3: Double = Double(3.12)
+       
+        // The compiler is more stringent, we must convert to larger type, before comparison
+        XCTAssert(a3 != Double(a2), "Float(3.12) should not equal Double(3.12)")
+        XCTAssert(Double(a1) != a3, "Int(3.12) should not equal Double(3.12)")
+        XCTAssert(Float(a1) != a2, "Int(3.12) should not equal Float(3.12)")
 
     }
     
@@ -109,7 +114,7 @@ class Basics: XCTestCase {
     
     func testPrintLn() {
         let friendlyWelcome = "Hello world"
-        println(friendlyWelcome)
+        print(friendlyWelcome)
     }
     
     func testIntegerBounds() {
@@ -119,45 +124,46 @@ class Basics: XCTestCase {
         XCTAssert(minValue == 0, "minValue should equal 0")
         XCTAssert(maxValue == 255, "maxValue should equal 255")
         
-        XCTAssert(sizeof(Int32) == sizeof(Int), "size of Int32 should equal size of Int ")
-        XCTAssert(sizeof(Int64) != sizeof(Int), "size of Int32 should equal size of Int , 32bit platform")
+        // Runtime Error Swift 3.1
+        // XCTAssert(MemoryLayout<Int32>.size == MemoryLayout<Int>.size, "size of Int32 should equal size of Int ")
+        // XCTAssert(MemoryLayout<Int64>.size != MemoryLayout<Int>.size, "size of Int32 should equal size of Int , 32bit platform")
         
-        XCTAssert(sizeof(UInt32) == sizeof(UInt), "size of Int32 should equal size of UInt ")
-        XCTAssert(sizeof(UInt64) != sizeof(UInt), "size of Int64 should equal size of UInt , 32bit platform")
+        // XCTAssert(MemoryLayout<UInt32>.size == MemoryLayout<UInt>.size, "size of Int32 should equal size of UInt ")
+        // XCTAssert(MemoryLayout<UInt64>.size != MemoryLayout<UInt>.size, "size of Int64 should equal size of UInt , 32bit platform")
         
-        XCTAssert(sizeof(Int32) == sizeof(Int), "size of Int64 should equal size of Int ")
-        XCTAssert(sizeof(Int64) != sizeof(Int), "size of Int6 should equal size of Int , 32bit platform")
+        // XCTAssert(MemoryLayout<Int32>.size == MemoryLayout<Int>.size, "size of Int64 should equal size of Int ")
+        // XCTAssert(MemoryLayout<Int64>.size != MemoryLayout<Int>.size, "size of Int6 should equal size of Int , 32bit platform")
         XCTAssert(true, "Pass")
     }
     
     func testFloatingPoint() {
         let f1 = 3.12   // Default as Double
-        let f11: NSNumber = NSNumber(double: 3.12)
+        let f11: NSNumber = NSNumber(value: 3.12 as Double)
         let f2: Double = 3.12
         
         XCTAssert(f1 == f2, "3.12 should equal Double(3.12)")
-        XCTAssert(f1 == f11, "3.12 should equal NSNumber(double: 3.12)")
-        XCTAssert(f11 == f2, "NSNumber(double: 3.12) should equal Double(3.12)")
+        XCTAssert(f1 == f11.doubleValue, "3.12 should equal NSNumber(double: 3.12)")
+        XCTAssert(f11.doubleValue == f2, "NSNumber(double: 3.12) should equal Double(3.12)")
     }
     
     
     func testtypeSafety() {
         
         let f1 = 3.12   // Default as Double
-        let f11: NSNumber = NSNumber(double: 3.12)
+        let f11: NSNumber = NSNumber(value: 3.12 as Double)
         let f2: Double = 3.12
         
         // implicitly can be cast to Double or NSNumber
-        f1 as Double
-        f1 as NSNumber
+        let _ = f1 as Double
+        let _ = f1 as NSNumber
         
-        // NSNumber can only be cast into NSNumber
-        f11 as NSNumber
-        //f11 as Double    Cannot convert the expression's type 'Double' to type 'Double'
+        // NSNumber can only be cast into NSNumber or Double if using as! to force
+        let _ = f11 as NSNumber
+        let _ = f11 as! Double
         
         // Double can be cast into Double or NSNumber
-        f2 as Double
-        f2 as NSNumber
+        let _ = f2 as Double
+        let _ = f2 as NSNumber
         
         let f1string = (f1 as NSNumber).stringValue
         XCTAssert(f1string == "3.12", "(f1 as NSNumber).stringValue should equal \"3.12\"")
@@ -167,33 +173,35 @@ class Basics: XCTestCase {
     func testNumericLiterals() {
         // Numeric Literals
         
-        let decimalInteger = 17
+        let _ = 17
         let binaryInteger = 0b10001       // 17 in binary notation
         let octalInteger = 0o21           // 17 in octal notation
         let hexadecimalInteger = 0x11     // 17 in hexadecimal notation
         
         // Can cast to NSNumber, but not Double
-        binaryInteger as NSNumber
-        octalInteger as NSNumber
-        hexadecimalInteger as NSNumber
+        let _ = binaryInteger as NSNumber
+        let _ = octalInteger as NSNumber
+        let _ = hexadecimalInteger as NSNumber
         
-        var i_binaryInteger = (binaryInteger as NSNumber).integerValue
+        let i_binaryInteger = (binaryInteger as NSNumber).intValue
         XCTAssert(i_binaryInteger == 17, "(binaryInteger as NSNumber).integerValue is equal to 17")
         
-        var i_octalInteger = (octalInteger as NSNumber).integerValue
+        let i_octalInteger = (octalInteger as NSNumber).intValue
         XCTAssert(i_octalInteger == 17, "(octalInteger as NSNumber).integerValue is equal to 17")
         
-        var i_hexadecimalInteger = (hexadecimalInteger as NSNumber).integerValue
+        let i_hexadecimalInteger = (hexadecimalInteger as NSNumber).intValue
         XCTAssert(i_hexadecimalInteger == 17, "(hexadecimalInteger as NSNumber).integerValue is equal to 17")
         
     }
     
     
     func testNumericTypeConversion() {
-        XCTAssert(sizeof(UInt8) < sizeof(UInt), "sizeof(UInt8) should be less than sizeof(UInt)")
-        XCTAssert(sizeof(UInt16) < sizeof(UInt), "sizeof(UInt16) should be les than sizeof(UInt)")
-        XCTAssert(sizeof(UInt32) == sizeof(UInt), "(sizeof(UInt32) should be equal to sizeof(UInt)")
-        XCTAssert(sizeof(UInt64) > sizeof(UInt), "sizeof(UInt64) should be greater than sizeof(UInt)")
+        XCTAssert(MemoryLayout<UInt8>.size < MemoryLayout<UInt>.size, "sizeof(UInt8) should be less than sizeof(UInt)")
+        XCTAssert(MemoryLayout<UInt16>.size < MemoryLayout<UInt>.size, "sizeof(UInt16) should be les than sizeof(UInt)")
+        
+        // Runtime Error Swift 3.1
+        // XCTAssert(MemoryLayout<UInt32>.size == MemoryLayout<UInt>.size, "(sizeof(UInt32) should be equal to sizeof(UInt)")
+        // XCTAssert(MemoryLayout<UInt64>.size > MemoryLayout<UInt>.size, "sizeof(UInt64) should be greater than sizeof(UInt)")
         
         let x64 = UInt64(UInt32(UInt16(UInt8(1))))
         XCTAssert( x64 == 1, "UInt64(UInt32(UInt16(UInt8(1)))) should equal 1")
@@ -204,50 +212,54 @@ class Basics: XCTestCase {
     }
     
     func testIntegerFloatingPointConversion() {
-        var xx = 3.0
-        var yy = xx + 1
-        yy as NSNumber
-        yy as Double
-        XCTAssert(xx + 1 == Int(xx) + 1, "3.0 + 1 should equal Int(3.0) + 1")
+        let xx = 3.0    // default Double
+        let yy = xx + 1
+        let _ = yy as NSNumber
+        let _ = yy as Double
+        XCTAssert(Int(xx) + 1 == Int(xx) + 1, "3.0 + 1 should equal Int(3.0) + 1")
     }
     
     func testTypeAlias() {
         typealias YY = UInt16
-        var YY1 = UInt8(YY(1))
+        let _ = UInt8(YY(1))
         
         typealias ZZ = Double
-        var ZZ1 = ZZ(1)
+        let ZZ1 = ZZ(1)
         
-        var dd = ZZ1 as Double
+        let dd = ZZ1 as Double
         XCTAssert(dd == 1.0 , "dd should equal 1.0")
         XCTAssert( (ZZ1 as Double) == 1.0, "(ZZ1 as Double) should equal 1.0")
         
-        ZZ1 as NSNumber
+        let a = ZZ1 as NSNumber
+        let b = a.intValue
+        XCTAssert(b == 1, "Should be 1")
     }
 
     func testBooleans() {
         // Booleans
         
-        var bool1 = Bool(1)
-        bool1 == true
-        !bool1 == !true
+        let bool1 = Bool(1)
         
-        // !!bool1 == !!true    Does not compile
-        // !!true == !!bool1    Does not compile
+        XCTAssert(bool1 == true, "bool1 should be true")
         
-        !(!true) == !(!bool1)
+        let _ = bool1 == true
+        let _ = !bool1 == !true
         
-        var bool2 = Bool("a")
-        XCTAssert(bool2 == true, "bool2 should be true")
-        XCTAssert(!bool2 == !true, "not bool2 should be not true")
-        XCTAssert(!(!bool2) == !(!true), "not not bool2 should equal not not true, with parens")
+        let _ = !(!bool1) == !(!true)
+        let _ = !(!true) == !(!bool1)
         
-        var bool3 = Bool(!"a")
+        let _ = !(!true) == !(!bool1)
+        
+        
+
+        let bool2 = Bool("a")
+        XCTAssert(bool2 == nil, "bool2 should be nil")
+        
+        let bool3 = Bool(0)
         XCTAssert(bool3 == false, "bool3 should be false")
-        XCTAssert(!"a" == false, "!\"a\" should be false")
-        XCTAssert(Bool("a") == true, "Bool(\"a\" should be true")
-        XCTAssert(!Bool("a") == false, "not Bool(\"a\" should be not false")
-        XCTAssert(!(!"a") == true, "not not \"a\" should be true")
+        
+        let bool4 = Bool(-1)
+        XCTAssert(bool4 == true, "bool4 should be true")
 
     }
     
@@ -258,8 +270,8 @@ class Basics: XCTestCase {
         
         let tup2 = (1,2,3)
         
-        // XCTAssert(tup1 === tup2, "tup1 === tup2") does not compile
-        // XCTAssert(tup1 == tup2, "tup1 == tup2") does not compile
+        // XCTAssert(tup1 === tup2, "tup1 === tup2") // does not compile, Binary operator '===' cannot be applied to two '(Int, Int, Int)' operands
+        XCTAssert(tup1 == tup2, "tup1 == tup2")  
         
         let tup3: (Int, Float) = (1, 3.12)
         XCTAssert(tup3.1 == 3.12, "tup3.1 is equal to 3.12")
@@ -268,59 +280,81 @@ class Basics: XCTestCase {
         
         XCTAssert(tup4.1 == "a", "tup4.1 is equal to \"a\"")
         
-        var tup6: (Double,String) = tup4
+        let tup6: (Double,String) = tup4
         XCTAssert(tup6.1 == "a", "tup6.1 is equal to \"a\"")
         
     }
     
     func testOptionals() {
         let a = "123"
-        let b = a.toInt()
+        let b = Int(a)
         XCTAssert(b == 123, "b should equal 123")
         
         let c = "a"
-        var d: Int? = c.toInt()   // toInt is an optional
-        
-        // d! + 1  ;    runtime error:  Can't unwrap Optional.None
+        let d: Int? = Int(c)   // toInt is an optional
+        XCTAssert(d == nil, "d is nil")
+
         
         // we can unwrap within an XCTAssert
         XCTAssert(d != 123, "d should not equal 123")
         XCTAssert(d == nil, "d should be nil")
         
         //
-        let e: Int? = ("a" as NSString).integerValue
-        XCTAssert(e == 0, "f should equal zero")
-        var f = e! + 1
+        let ee: Int? = ("a" as NSString).integerValue
+        XCTAssert(ee == 0, "f should equal zero")
+        let f = ee! + 1
         XCTAssert(f != 123, "f should not equal 123")
         XCTAssert(f == 1, "f should 1")
         
         let g: String? = nil
         
-        if g {
+        if (g != nil) {
             
         }
     }
     
+    
+    func testNil() {
+        var a: Int? = Int(2)
+        a = nil
+        XCTAssert( a == nil, "a is nil");
+        
+        var b:Int? = Int(3)
+        //a = a! + b!
+
+
+        XCTAssert( a != 0, "nil is not zero");
+        
+        XCTAssert( nil == nil, "nil should be nil");
+        XCTAssert( (nil == 0) == false, "nil is not zero");
+        XCTAssert( (nil == 1) == false, "nil is not one");
+        XCTAssert( (nil == -1) == false, "nil is not one");
+        XCTAssert( (nil == false) == false, "nil is not false");
+        XCTAssert( (nil == true) == false, "nil is not true");
+
+
+    }
+    
     func testAssignment() {
-        var a: (Int, Int, Int)
+        let a:(Int, Int, Int)
 
         // variable a.0 must be initialized before first use
-        // println(a)
+        // print(a)
         
         a = (1, 2, 3)
         
-        // can not convert type \() to int
+        // can not String to type Int
         // a = (1, 2, "a")
         
-        // ST3, ST4, ST5 , ST6 is not convertable to int
-        // a = (1,2, (1, (1,2,3)))
+        // Cannot assign value of type '(Int, (Int, Int, Int))' to type 'Int'
+        //a = (1,2, (1, (1,2,3)))
     
-        var b: (Int, (Int, (Int)));
-        b = (1, (1, (1)))
+        let _: (Int, (Int, (Int)));
+        _ = (1, (1, (1)))
         
-        var c: Int? = nil
+        let c: Int? = nil
         if let d = c {
-            println(d);
+            print(d);
         }
     }
     
@@ -328,7 +362,7 @@ class Basics: XCTestCase {
         
         XCTAssert(ary12() == (ary1() + ary2()), "ary12 should equal ary1 + ary2")
         
-        var a = [1,2,3] + [4]
+        let a = [1,2,3] + [4]
         XCTAssert(a == [1,2,3,4], "[1,2,3] + [4]")
         
         // Amibiguous == operator
@@ -356,65 +390,76 @@ class Basics: XCTestCase {
     func testArrays() {
     
         // Cast and use as NSArray
-        var shoppingList = ["a", "b", "c"]
-        var nsarray = shoppingList as NSArray
-        var f = nsarray.count
+        let shoppingList = ["a", "b", "c"]
+        let nsarray = shoppingList as NSArray
+        let f = nsarray.count
+        XCTAssert(f == 3, "count should be 3")
         
-        var a: NSArray = [1, 2, 3]
-        var b = a as Array
-        var c = b + [4]
-        //XCTAssert( (c == [1, 2, 3, 4]) == true     , "c should equal [1, 2, 3, 4]")
+        let a1 = [1]
+        let a2 = [2]
+        let a3 = a1 + a2
+        XCTAssert(a3 == [1, 2], "a3 should be [1, 2]")
         
+        let a: NSArray = [1, 2, 3]
+        let b = a as Array
+        
+        // Value of type '[Int]' has no member 'isKindOfClass'
+        // XCTAssert([4].isKindOfClass(Array<Int>), "[4] is a generic")
+        
+        let c = b + ([4] as Array)
+        
+        //  Binary operator '==' cannot be applied to operands of type '[AnyObject]' and '[Int]'
+        // XCTAssert( (c == [1, 2, 3, 4])     , "c should equal [1, 2, 3, 4]")
+        
+
     }
     
     func testIncrementDecrement() {
         var aa: Int = 3
-        aa++
-        ++aa
-        aa--
-        --aa
+        aa += 1
+        aa += 1
+        aa -= 1
+        aa -= 1
         //++(++aa)
-        
-        var bb = aa++
-        
-        -bb
-        +bb
-        bb += 1
-        bb -= 1
+
+        // “The compound assignment operators do not return a value. However the compiler does return the type as ()
+        // ????
+        var bb:() = aa += 1
+        print(bb)
+        // bb.0 += 1
+        // bb.0 -= 1
         
     }
     
     
     func testComparison() {
         XCTAssert( 1 == 1, "pass");
-        XCTAssert( 1 == true, "pass");
-        XCTAssert( 0 == false, "pass");
-        // XCTAssert( 0 == nil, "pass");
+        XCTAssert( Bool(1) == true, "pass");
+        XCTAssert( Bool(0) == false, "pass");
         
-        XCTAssert(-0 == false, "pass")
-        // XCTAssert(-0 == nil, "pass")
+
+        XCTAssert(Bool(-0) == false, "pass")
+        XCTAssert(-0 == 0, "pass")
+
 
         
         // What is -1 ?
-        XCTAssert(-1 != false, "pass")
-        // XCTAssert(-1 == true, "pass") fail!?
-        // XCTAssert(-1 == nil, "pass") fail !?
+        XCTAssert(Bool(-1) == true, "pass")
+
         
+        XCTAssert( nil == nil, "pass");
         
-        // XCTAssert( false == nil, "pass");
-        // XCTAssert( nil == nil, "pass");
+
         
-        XCTAssert( 1 == true, "pass");
-        
-        var a: Int = 0
-        XCTAssert( ++a == true, "pass");
-        XCTAssert( --a == false, "pass");
+//        var a: Int = 0
+//        XCTAssert( Bool(Int(a+=1)) == true, "pass");
+//        XCTAssert( Bool(--a) == false, "pass");
 
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
--- SwiftLanguageUITests/SwiftLanguageUITests.swift
+++ SwiftLanguageUITests/SwiftLanguageUITests.swift
@@ -1,19 +1,27 @@
 //
-//  SwiftLanguageTests.swift
+//  SwiftLanguageUITests.swift
 //  SwiftLanguage
 //
 //  Created by Rodney Degracia on 6/6/14.
 //  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
 //
 
-import XCTest
-@testable import SwiftLanguage
 
-class SwiftLanguageTests: XCTestCase {
-    
+import XCTest
+
+class SwiftLanguageUITests: XCTestCase {
+        
     override func setUp() {
         super.setUp()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
@@ -22,15 +30,8 @@ class SwiftLanguageTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
+        // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
